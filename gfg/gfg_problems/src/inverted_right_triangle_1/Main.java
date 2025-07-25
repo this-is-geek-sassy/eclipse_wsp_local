3dd6c1b0624b3d7beb1af7467b6e2e75432a152a
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2025-07-25 13:11:18 +0530

    inverted right triangle solved

---
+++ gfg/gfg_problems/src/inverted_right_triangle_1/Main.java
@@ -0,0 +1,12 @@
+package inverted_right_triangle_1;
+
+// Link: https://www.geeksforgeeks.org/problems/triangle-pattern/1
+public class Main {
+	
+	public static void main(String[] args) {
+		Solution1 solution1 = new Solution1();
+		
+		solution1.printTriangle(6);
+	}
+
+}
---
+++ gfg/gfg_problems/src/inverted_right_triangle_1/Solution1.java
@@ -0,0 +1,14 @@
+package inverted_right_triangle_1;
+
+public class Solution1 {
+	
+	void printTriangle(int n) {
+
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=n-i+1; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
